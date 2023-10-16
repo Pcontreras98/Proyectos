7a62a84d2a1d2665a9ddd8e502d0022570343d77
Pcontreras98 <martin_contrerasn@hotmail.com>
2023-10-15 20:25:47 -0500

    cambios

--- FestivalMusica/gulpfile.js
+++ FestivalMusica/gulpfile.js
@@ -1,9 +1,17 @@
-const { src,dest,watch } = require("gulp");
+const { src,dest,watch,parallel } = require("gulp");
+
+//css
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require('gulp-plumber');
 
+//Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
 function css(done) {
-    
+
     src('src/scss/**/*.scss')      //Identificar el archivo SASS
         .pipe( plumber())
         .pipe( sass())            //Compilarlo
@@ -11,6 +19,40 @@ function css(done) {
 
     done(); //Callback que avisa a gulp cuando llegamos al final
 }
+
+function imagenes(done){
+    const opciones ={
+        optimizationLevel: 3
+    }
+    
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache( imagemin(opciones)))
+        .pipe(dest('build/img'))    
+    done();
+}
+
+function versionWebp( done ){
+
+    const opciones = {
+        quality: 50
+    }
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones))
+        .pipe(dest('build/img'))
+    done();
+}
+function versionAvif( done ){
+
+    const opciones = {
+        quality: 50
+    }
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones))
+        .pipe(dest('build/img'))
+    done();
+} 
 
 function dev(done){
 
@@ -20,4 +62,7 @@ function dev(done){
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel( imagenes, versionWebp, versionAvif, dev);
